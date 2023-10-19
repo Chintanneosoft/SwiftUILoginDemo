99bd547a4d0789c8a6fe63efc904a945bad92ead
Chintanneosoft <chintan.rajgor@neosoftmail.com>
2023-10-19 14:09:29 +0530

    Login Demo

--- SwiftUILoginDemo/ContentView.swift
+++ SwiftUILoginDemo/ContentView.swift
@@ -8,12 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var username = ""
+    @State var password = ""
+    @State var showAlert:Bool = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Login")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.top, -100.0)
+            TextFieldsView(lblText: "UserName", placeHolder: "Enter UserName", textFieldText: $username)
+            TextFieldsView(lblText: "Password", placeHolder: "Enter Password", textFieldText: $password)
+            Button(action: {
+              self.showAlert = true
+            }) {
+                Text("Submit").foregroundColor(.white)
+                    .padding(.all, 10.0)
+            }
+            .border(.black).background(.gray)
+            .alert(isPresented: self.$showAlert) {
+              Alert(title: Text("Login Hogaya"), message: Text("Aisa Boluga socha thana"))
+            }
         }
         .padding()
     }
@@ -22,5 +37,20 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct TextFieldsView: View {
+    var lblText:String
+    var placeHolder:String
+    @Binding var textFieldText:String
+    var body: some View {
+        HStack{
+            Text(lblText)
+                .padding(.leading, 30.0).frame(width: 150)
+            TextField(placeHolder, text: $textFieldText)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.trailing, 30.0)
+        }.padding()
     }
 }
