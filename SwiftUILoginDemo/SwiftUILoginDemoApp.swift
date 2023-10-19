c8389d376739b3c0e64e79b1b6c96408f3fb2f4f
Chintanneosoft <chintan.rajgor@neosoftmail.com>
2023-10-19 11:20:18 +0530

    Initial Commit

---
+++ SwiftUILoginDemo/SwiftUILoginDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILoginDemoApp.swift
+//  SwiftUILoginDemo
+//
+//  Created by Neosoft on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILoginDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
